488afc074b62ef32acc64336eb9c2f4d08f763b5
Ada Joule <ada.fulmina@gmail.com>
2019-04-24 10:20:34 +0700

    Finish DeckGenerator

--- src/main/java/alchemagis/deckgenerator/DeckGenerator.java
+++ src/main/java/alchemagis/deckgenerator/DeckGenerator.java
@@ -4,22 +4,31 @@ import java.util.List;
 
 import alchemagis.deckgenerator.metric.Metric;
 import alchemagis.magic.Card;
+import alchemagis.magic.CardPool;
 import alchemagis.magic.Deck;
 import alchemagis.magic.MagicConstants;
 
 public final class DeckGenerator {
 
+    private CardPool cardPool;
     private List<Metric> metrics;
+    private List<Double> utilityScores;
 
     public Deck generateDeck() {
-        return null;
+        return this.generateDeck(List.of(this.cardPool.getRandomCard()));
     }
 
     public Deck generateDeck(List<Card> startingCards) {
         final Deck generatedDeck = new Deck(startingCards);
 
         while (generatedDeck.size() < MagicConstants.MIN_DECK_SIZE) {
-            
+            Card maxUtilityCard = cardPool.stream().max(
+                (c1, c2) ->
+                    Double.compare(
+                        this.getUtilityScore(generatedDeck, c1),
+                        this.getUtilityScore(generatedDeck, c2))).
+            get();
+            generatedDeck.add(maxUtilityCard);
         }
 
         return generatedDeck;
