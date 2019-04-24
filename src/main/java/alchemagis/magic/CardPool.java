bf74a2b7ccca186a7c4c0836621b75115989920f
Ada Joule <ada.fulmina@gmail.com>
2019-04-24 10:31:03 +0700

    Add get random card from pool

--- src/main/java/alchemagis/magic/CardPool.java
+++ src/main/java/alchemagis/magic/CardPool.java
@@ -1,6 +1,9 @@
 package alchemagis.magic;
 
 import java.util.List;
+import java.util.stream.Stream;
+
+import alchemagis.util.RandomUtil;
 
 public final class CardPool {
 
@@ -14,6 +17,14 @@ public final class CardPool {
 
     public List<Card> getCards() {
         return this.cards;
+    }
+
+    public Stream<Card> stream() {
+        return this.cards.stream();
+    }
+
+    public Card getRandomCard() {
+        return cards.get(RandomUtil.getRandomInt(cards.size()));
     }
 
 }
