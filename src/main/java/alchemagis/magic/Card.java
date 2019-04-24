2231ed29af462d44658660a033d6c8a9b164bfa1
Ada Joule <ada.fulmina@gmail.com>
2019-04-24 10:30:37 +0700

    Remove cost effectiveness and synergies from card class

--- src/main/java/alchemagis/magic/Card.java
+++ src/main/java/alchemagis/magic/Card.java
@@ -8,19 +8,8 @@ public class Card {
 
     private String name;
 
-    private double costEffectiveness;
-    private List<Synergy> synergies;
-
     public String getName() {
         return this.name;
-    }
-
-    public double getCostEffectiveness() {
-        return this.costEffectiveness;
-    }
-
-    public List<Synergy> getSynergies() {
-        return this.synergies;
     }
 
 }
