cb0fb4e18f8ce2a08e75cb23436456f4cec4344f
Ada Joule <ada.fulmina@gmail.com>
2019-04-24 16:01:35 +0700

    Stub Main

--- src/main/java/alchemagis/Main.java
+++ src/main/java/alchemagis/Main.java
@@ -1,9 +1,18 @@
 package alchemagis;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import alchemagis.deckgenerator.DeckGenerator;
+import alchemagis.deckgenerator.metric.Metric;
+
 public class Main {
 
     public static void main(String[] args) {
+        List<Metric> metrics = new ArrayList<>();
+        // TODO load metrics
 
+        //DeckGenerator deckGenerator = new DeckGenerator();
     }
 
 }
