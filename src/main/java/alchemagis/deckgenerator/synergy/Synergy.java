db63ad29ce5cbc685007def79153fd59c6cf6daf
Ada Joule <ada.fulmina@gmail.com>
2019-04-24 11:02:16 +0700

    Finish Synergy interface

--- src/main/java/alchemagis/deckgenerator/synergy/Synergy.java
+++ src/main/java/alchemagis/deckgenerator/synergy/Synergy.java
@@ -14,12 +14,6 @@ public abstract class Synergy {
         this.synergyWeight = synergyWeight;
     }
 
-    public static final double getSynergyScore(Card card1, Card card2) {
-        return card1.getSynergies().stream().
-            mapToDouble(s -> s.getScore(card2)).
-            sum();
-    }
-
     public final double getScore(Card card) {
         return this.synergyWeight * this.getRawScore(card);
     }
