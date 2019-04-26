34ecf3f0dd1635951ab9afa2841766fbc2afd232
Ada Joule <ada.fulmina@gmail.com>
2019-04-26 13:20:22 +0700

    Finalise Card class

--- src/main/java/alchemagis/magic/Card.java
+++ src/main/java/alchemagis/magic/Card.java
@@ -2,11 +2,27 @@ package alchemagis.magic;
 
 import java.util.List;
 
-import alchemagis.deckgenerator.synergy.Synergy;
-
 public class Card {
 
     private String name;
+
+    private String[] colors;
+
+    private String manaCost;
+
+    private double convertedManaCost;
+
+    private String[] supertypes;
+
+    private String[] types;
+
+    private String[] subtypes;
+
+    private String power;
+
+    private String toughness;
+
+    private String text;
 
     public String getName() {
         return this.name;
