f5cb1787f76b0afbfcf686439cf828a410d3188b
Ada Joule <ada.fulmina@gmail.com>
2019-08-28 18:02:38 +0700

    Add deck list sorting

--- src/main/java/alchemagis/magic/Deck.java
+++ src/main/java/alchemagis/magic/Deck.java
@@ -2,9 +2,12 @@ package alchemagis.magic;
 
 import java.util.Collection;
 import java.util.Collections;
+import java.util.List;
+import java.util.ArrayList;
 import java.util.stream.Stream;
 
 import com.google.common.collect.Multiset;
+import com.google.common.collect.Multisets;
 import com.google.common.collect.HashMultiset;
 
 public class Deck {
@@ -35,14 +38,16 @@ public class Deck {
         return this.cards.stream();
     }
 
-    public Collection<Card> getCards() {
-        return Collections.unmodifiableCollection(this.cards);
+    public Multiset<Card> getCards() {
+        return Multisets.unmodifiableMultiset(this.cards);
     }
 
     @Override
     public String toString() {
         StringBuilder str = new StringBuilder();
-        for (Multiset.Entry<Card> cardEntry : this.cards.entrySet()) {
+        List<Multiset.Entry<Card>> sortedEntry = new ArrayList<>(this.cards.entrySet());
+        Collections.sort(sortedEntry, Deck::compareType);
+        for (Multiset.Entry<Card> cardEntry : sortedEntry) {
             str.append(cardEntry.getCount());
             str.append(' ');
             str.append(cardEntry.getElement().getName());
@@ -51,4 +56,22 @@ public class Deck {
         return str.toString();
     }
 
+    private static final List<String> typeOrder = List.of("creature", "planeswalker", "instant", "sorcery", "artifact", "enchantment", "land");
+    private static final int compareType(Multiset.Entry<Card> entry1, Multiset.Entry<Card> entry2) {
+        Card card1 = entry1.getElement();
+        Card card2 = entry2.getElement();
+        for (String type : typeOrder) {
+            boolean contain1 = card1.getTypes().contains(type);
+            boolean contain2 = card2.getTypes().contains(type);
+            if (contain1 && !contain2)
+                return -1;
+            else if (contain2 && !contain1)
+                return 1;
+        }
+        int compareCMC = Integer.compare(card1.getConvertedManaCost(), card2.getConvertedManaCost());
+        if (compareCMC != 0)
+            return compareCMC;
+        else
+            return card1.getName().compareToIgnoreCase(card2.getName());
+    }
 }
