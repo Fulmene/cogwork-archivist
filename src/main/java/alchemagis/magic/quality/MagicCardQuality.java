da6c5fea858dfd24dc55c44d9ee6b90c5c69d2e9
Ada Joule <ada.fulmina@gmail.com>
2019-11-15 14:46:43 +0700

    Add a brand new MagicCardQuality class to represent quality
    Formerly "active synergy"

---
+++ src/main/java/alchemagis/magic/quality/MagicCardQuality.java
@@ -0,0 +1,12 @@
+package alchemagis.magic.quality;
+
+import java.util.List;
+
+public abstract class MagicCardQuality {
+
+    public static List<MagicCardQuality> parseQualities(String qualities) {
+        // TODO stub
+        return List.of();
+    }
+
+}
