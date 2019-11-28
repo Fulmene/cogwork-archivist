2abf31269058cd9d3a4581c45fc40ea05ccbfa54
Ada Joule <ada.fulmina@gmail.com>
2019-11-28 15:07:02 +0700

    Add the skeleton of MagicCardQuality class

--- src/main/java/alchemagis/magic/quality/MagicCardQuality.java
+++ src/main/java/alchemagis/magic/quality/MagicCardQuality.java
@@ -1,6 +1,27 @@
 package alchemagis.magic.quality;
 
+import java.util.stream.Collectors;
+import java.util.List;
+
 public final class MagicCardQuality {
 
+    private final ColorQuality colorQuality;
+    private final TypeQuality typeQuality;
+    private final ManaCostQuality manaCostQuality;
+    private final StatQuality statQuality;
+    private final List<MagicCardQualityType> otherQualities;
+
+    public MagicCardQuality(String qualities) {
+        // TODO parse quality string
+        this.qualities = List.of();
+    }
+
+    public final List<MagicCardQualityType> getQualities() {
+        return this.qualities;
+    }
+
+    public final List<MagicCardQualityType> getQualitiesOfType(Class qualityType) {
+        return this.qualities.stream().filter(qualityType::isInstance).collect(Collectors.toList());
+    }
 
 }
