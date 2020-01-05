1795766d418e024bc70dff00fb1275c9b00b2949
Ada Joule <ada.fulmina@gmail.com>
2020-01-05 16:49:17 +0700

    Use generic instead of returning void for visitor

--- src/main/java/alchemagis/magic/quality/MagicCardQualityType.java
+++ src/main/java/alchemagis/magic/quality/MagicCardQualityType.java
@@ -2,10 +2,12 @@ package alchemagis.magic.quality;
 
 public abstract class MagicCardQualityType {
 
-    public abstract void accept(Visitor v);
+    public abstract <T> T accept(Visitor<T> visitor);
 
-    public static interface Visitor {
-        default void visitStat(StatQuality stat) {}
+    public static interface Visitor<T> {
+        T visitColor(ColorQuality color);
+        T visitStat(StatQuality stat);
+        T visitType(TypeQuality type);
     }
 
 }
