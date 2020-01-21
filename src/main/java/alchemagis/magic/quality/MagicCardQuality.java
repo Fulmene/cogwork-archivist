5cf27b1db9fb2b36548221fef035f09243ce06d5
Ada Joule <ada.fulmina@gmail.com>
2020-01-21 15:54:22 +0700

    Provisional MagicCardQuality generation

--- src/main/java/alchemagis/magic/quality/MagicCardQuality.java
+++ src/main/java/alchemagis/magic/quality/MagicCardQuality.java
@@ -3,21 +3,23 @@ package alchemagis.magic.quality;
 import java.util.stream.Collectors;
 import java.util.List;
 
+import alchemagis.magic.Card;
+
 public final class MagicCardQuality {
 
     private final List<MagicCardQualityType> qualities;
 
-    public MagicCardQuality(String qualities) {
-        // TODO parse quality string
-        this.qualities = List.of();
+    public MagicCardQuality(Card card) {
+        this.qualities = List.of(
+            new ColorQuality(card.getColors()),
+            new ManaCostQuality(card.getManaCost()),
+            new StatQuality(card.getPower(), card.getToughness()),
+            new TypeQuality(card.getSupertypes(), card.getTypes(), card.getSubtypes())
+        );
     }
 
     public final List<MagicCardQualityType> getQualities() {
         return this.qualities;
-    }
-
-    public final List<MagicCardQualityType> getQualitiesOfType(Class<? extends MagicCardQualityType> qualityType) {
-        return this.qualities.stream().filter(qualityType::isInstance).collect(Collectors.toList());
     }
 
 }
