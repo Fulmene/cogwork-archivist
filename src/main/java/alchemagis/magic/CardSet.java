ff7dd96074efd73c1f946adfd1ccb7cc847b0e86
Ada Joule <ada.fulmina@gmail.com>
2020-04-16 21:35:44 +0700

    Use proper method for CardSet

--- src/main/java/alchemagis/magic/CardSet.java
+++ src/main/java/alchemagis/magic/CardSet.java
@@ -1,6 +1,9 @@
 package alchemagis.magic;
 
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.Set;
+import java.util.HashSet;
 
 import com.fasterxml.jackson.annotation.JsonAutoDetect;
 import com.fasterxml.jackson.annotation.JsonCreator;
@@ -17,7 +20,7 @@ public class CardSet {
     @JsonCreator(mode = JsonCreator.Mode.PROPERTIES)
     public CardSet(@JsonProperty("name") String name, @JsonProperty("cards") Card[] cards) {
         this.name = name;
-        this.cards = Set.of(cards);
+        this.cards = Collections.unmodifiableSet(new HashSet<Card>(Arrays.asList(cards)));
     }
 
     public String getName() {
