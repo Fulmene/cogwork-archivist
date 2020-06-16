d8894257e4329b58b640d96191a2f340f87e52d6
Ada Joule <ada.fulmina@gmail.com>
2020-06-16 21:37:29 +0700

    Add parsing function for ability quality

--- src/main/java/mtgcogwork/magic/quality/QualityParser.java
+++ src/main/java/mtgcogwork/magic/quality/QualityParser.java
@@ -4,6 +4,8 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import mtgcogwork.magic.Action;
+
 public final class QualityParser {
 
     public static List<MagicCardQuality> parseQualities(String qualitiesString) {
@@ -15,6 +17,12 @@ public final class QualityParser {
     public static MagicCardQuality parseQuality(String qualityString) {
         String[] qualityArgs = qualityString.split("[(),]");
         switch (qualityArgs[0]) {
+            case "ability":
+                return new AbilityQuality(
+                        AbilityQuality.AbilityType.valueOf(qualityArgs[1].toUpperCase()),
+                        Arrays.stream(qualityArgs[2].split(";")).map(Action::getInstance).collect(Collectors.toList()),
+                        Arrays.stream(qualityArgs[3].split(";")).map(Action::getInstance).collect(Collectors.toList()),
+                        List.of(qualityArgs[4].split(";")));
             default: throw new QualityFormatException(qualityString);
         }
     }
