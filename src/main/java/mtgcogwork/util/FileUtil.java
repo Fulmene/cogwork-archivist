af85bf9bd8c7093d57b632634ff72a9126d2ef0a
Ada Joule <ada.fulmina@gmail.com>
2020-06-21 03:04:02 +0700

    Clean imports

--- src/main/java/mtgcogwork/util/FileUtil.java
+++ src/main/java/mtgcogwork/util/FileUtil.java
@@ -4,7 +4,6 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.net.URL;
 import java.nio.channels.Channels;
-import java.nio.channels.ReadableByteChannel;
 import java.nio.file.Path;
 import java.util.List;
 
